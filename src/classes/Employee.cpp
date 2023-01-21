23f9616a328f60d7fdb9a6a4cad04f28eb3b668d
wtargosi <wiktor.targosinski.stud@pw.edu.pl>
2023-01-21 13:05:43 +0100

    clear

--- src/classes/Employee.cpp
+++
@@ -1,8 +0,0 @@
-#include "headers/Employee.h"
-
-Employee::Employee(std::string first_name, std::string last_name) : 
-    Person::Person(first_name, last_name) {}
-
-void Employee::addEntry(Patient& patient, std::string action) {
-    patient.addEntry(Entry(action,this));
-}
